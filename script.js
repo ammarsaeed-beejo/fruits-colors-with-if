2bdf07c8c8aea9725ce1ea90442667c3d8b9f76e
ammarsaeed-beejo <ammarsaeed.art@gmail.com>
2021-04-23 18:32:48 +0300

    commit2

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let value = prompt("enter color");
+
+if(value == "red"){
+
+    alert ("apple");}
+
+else if(value == "yellow"){
+
+    alert ("banana");}
+
+else if(value == "green"){
+
+    alert("grass");}
+
+else if(value == "circle"|| value == "rectangle" || value == "sqaure"){
+
+    alert ("shape");}
+
+else{
+
+  alert("i dont know ;)")}
